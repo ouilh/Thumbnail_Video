b9605db49c82efcb7caa5bcebfcfde09cbbb6978
yassine <118019022+ouilh@users.noreply.github.com>
2023-10-12 16:40:18 +0100

    Add files via upload

---
+++ audio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Coming Soon</title>
+    <style>
+        body {
+           /* Background color */
+           /* Text color */
+            text-align: center; /* Center-align text horizontally */
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Center-align text vertically */
+        }
+
+        h1 {
+            font-size: 36px; /* Adjust the font size as needed */
+        }
+
+        p {
+            font-size: 18px; /* Adjust the font size as needed */
+        }
+    </style>
+</head>
+<body style=" background-color: #0E1117; ">
+    <?php include("menu.php");?>
+    <div style="margin-top:-40%;text-align: center; align-items: center;  color: #FFFFFF; ">
+    <h1>Coming Soon</h1>
+    <p>We are working on something exciting! Stay tuned for updates.</p></div>
+</body>
+</html>
