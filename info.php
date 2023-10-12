d0b628b8ce62fbe5b7e090a138c782a51af13c76
yassine <118019022+ouilh@users.noreply.github.com>
2023-10-12 16:28:44 +0100

    Add files via upload

---
+++ info.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="icon" type="image/png" href="https://i.ibb.co/k0km76h/Logo.png">
+    <title>YouTube Downloader - Information</title>
+    <style>
+        body {
+           
+           
+           /* Center-align text horizontally */
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+           
+            
+        }
+        ul{
+            list-style-type: none;
+            color:#b3b3b3;
+        }
+      
+    </style>
+</head>
+<body style=" background-color: #0E1117;">
+    <?php include("menu.php") ?>
+    <div style=" align-items: center;height: 100vh; text-align: center; margin-top:-25%; ">
+    <h1 style="color:#333;">About YouTube Downloader</h1>
+
+    <p>Welcome to YouTube Downloader, your go-to destination for downloading YouTube thumbnails and videos. Our service provides a convenient way to access and save media from YouTube for various purposes, such as content creation, research, or personal enjoyment.</p>
+
+    <h2 style="color:#666;">Features:</h2>
+    <ul >
+        <li>Download YouTube thumbnails in different quality options.</li>
+        <li>Save YouTube videos to your device for offline viewing.</li>
+        <li>Easy-to-use and user-friendly interface.</li>
+    </ul>
+
+    <h2 style="color:#666;">Usage Guidelines:</h2>
+    <p>While we aim to provide a useful tool for accessing YouTube content, please note the following guidelines:</p>
+    <ul>
+        <li>Respect copyright and licensing laws when using downloaded content.</li>
+        <li>Downloading content from YouTube may be subject to YouTube's terms of service.</li>
+        <li>Use the downloaded content responsibly and for legal purposes.</li>
+    </ul>
+
+    <p>Thank you for using our service. If you have any questions or feedback, please feel free to contact us.</p>
+
+    <h2 style="color:#666;">Contact Us:</h2>
+    <p>If you have any inquiries or suggestions, you can reach us at <a href="mailto:yassine.ouialane@gmail.com">contact@Freethumbnail.com</a>.</p></div>
+</body>
+</html>
