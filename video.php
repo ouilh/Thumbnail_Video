ab7c605774e865c19082ac5e751dceaa85e7a7d0
yassine <118019022+ouilh@users.noreply.github.com>
2023-10-12 15:46:16 +0100

    Update video.php

--- video.php
+++ video.php
@@ -39,6 +39,8 @@ if (isset($_POST['submit'])) {
 <html lang="en">
 
 <head>
+  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2979288114136410"
+     crossorigin="anonymous"></script>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
