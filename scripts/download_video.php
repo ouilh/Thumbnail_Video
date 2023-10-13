f6dfa70d8712335ee2272a5d86894e20a7789fe0
yassine <118019022+ouilh@users.noreply.github.com>
2023-10-13 01:01:41 +0100

    Update download_video.php

--- scripts/download_video.php
+++ scripts/download_video.php
@@ -1,16 +1,17 @@
-<?php
 $downloadURL = urldecode($_GET['link']);
 $type = urldecode($_GET['type']);
 $title = urldecode($_GET['title']);
-$fileName = $title.'.'.$type;
-
+$fileName = $title . '.' . $type;
 
 if (!empty($downloadURL) && substr($downloadURL, 0, 8) === 'https://') {
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
     header("Content-Disposition: attachment;filename=\"$fileName\"");
     header("Content-Transfer-Encoding: binary");
+    
+    // Set the Content-Type header to specify the file type
+    header("Content-Type: video/mp4");
 
     readfile($downloadURL);
-
 }
+?>
